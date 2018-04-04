de06f03f4af0d22c30d514d53e210b984f5c29df
yannou38 <yann.ducruy@live.fr>
2018-04-04 08:48:35 +0200

    more javadoc

--- src/main/java/lib/DataframeColumn.java
+++ src/main/java/lib/DataframeColumn.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 /**
  *
  * @author marcof
+ * @param <E> The type of the column
  */
 public class DataframeColumn<E> {
 
@@ -17,34 +18,62 @@ public class DataframeColumn<E> {
     private String Label;
     private ArrayList<E> columnContents;
     
+    /**
+     *  Create a dataframe column from a set of data and a label
+     * @param Label the label of the column
+     * @param columnContents the data set of the column
+     */
     public DataframeColumn(String Label, ArrayList<E> columnContents) {
         this.Label = Label;
         this.columnContents = columnContents;
     }
 
+    /**
+     *  Create a dataframe column from an label and create an empty set
+     * @param Label the label of the column
+     */
     public DataframeColumn(String Label) {
         this.Label = Label;
         this.columnContents = new ArrayList<>();
     }
 
+    /**
+     *  Base constructor
+     */
     public DataframeColumn() {
         this.Label = "";
         this.columnContents = new ArrayList<>();
         
     }
     
+    /**
+     *  Getter
+     * @return the label
+     */
     public String getLabel() {
         return Label;
     }
 
+    /**
+     *  Setter
+     * @param Label the new label
+     */
     public void setLabel(String Label) {
         this.Label = Label;
     }
 
+    /**
+     *  Getter
+     * @return the data set
+     */
     public ArrayList<E> getColumnContents() {
         return columnContents;
     }
 
+    /**
+     *  Setter
+     * @param columnContents the new data set
+     */
     public void setColumnContents(ArrayList<E> columnContents) {
         this.columnContents = columnContents;
     }
